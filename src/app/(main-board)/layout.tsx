01107c45be9091ef28a34d174404ab74b2c990b8
Nitzan Karsh <nitzan0426@gmail.com>
2024-08-09 10:39:20 +0300

    ui updates

--- src/app/(main-board)/layout.tsx
+++ src/app/(main-board)/layout.tsx
@@ -1,11 +1,11 @@
 export default function MainBoardLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div className="flex justify-center items-center mt-20 w-full">
-        {children}
-      </div>
-    );
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex justify-center items-center mt-20 w-full p-6">
+      {children}
+    </div>
+  );
+}
